d69610ca869b07f1c87bf8619842e87ef684d0c1
yourfrienddhruv <yourfrienddhruv@gmail.com>
2017-12-05 12:05:03 +0530

    using latest version to support BCM2835 PI ZERO version

--- src/main/java/org/apache/pi/rover/rover/RoverApplication.java
+++ src/main/java/org/apache/pi/rover/rover/RoverApplication.java
@@ -1,17 +1,18 @@
 package org.apache.pi.rover.rover;
 
 import com.pi4j.io.gpio.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/")
 @RestController
 @SpringBootApplication
+@CrossOrigin
 public class RoverApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(RoverApplication.class);
     private GpioController gpio;
 
     private GpioPinDigitalOutput forward;
@@ -24,8 +25,75 @@ public class RoverApplication {
     }
 
     @GetMapping(path = "drive")
-    public String drive(@RequestParam String direction) {
+    public String drive(@RequestParam String direction) throws InterruptedException {
+        LOG.info("REQUEST  Direction = {}", direction);
+        if ("rf".equalsIgnoreCase(direction)) {
+            rightForward();
+        } else if ("rb".equalsIgnoreCase(direction)) {
+            rightBack();
+        } else if ("lf".equalsIgnoreCase(direction)) {
+            leftForward();
+        } else if ("lb".equalsIgnoreCase(direction)) {
+            leftBack();
+        } else if ("f".equalsIgnoreCase(direction)) {
+            forward();
+        } else if ("b".equalsIgnoreCase(direction)) {
+            backward();
+        }
+        LOG.info("RESPONSE Direction = {}", direction);
         return direction;
+    }
+
+    private void forward() throws InterruptedException {
+        initialize();
+        forward.low();
+        Thread.sleep(100);
+        forward.high();
+    }
+
+    private void backward() throws InterruptedException {
+        initialize();
+        backward.low();
+        Thread.sleep(100);
+        backward.high();
+    }
+
+    private void leftBack() throws InterruptedException {
+        left.low();
+        Thread.sleep(100);
+        backward.low();
+        Thread.sleep(300);
+        backward.high();
+        forward.low();
+        Thread.sleep(20);
+        forward.high();
+        left.high();
+    }
+
+    private void leftForward() throws InterruptedException {
+        initialize();
+        left.low();
+        Thread.sleep(100);
+        forward.low();
+        Thread.sleep(300);
+        forward.high();
+        backward.low();
+        Thread.sleep(50);
+        backward.high();
+        left.high();
+    }
+
+    private void rightBack() throws InterruptedException {
+        initialize();
+        right.low();
+        Thread.sleep(100);
+        backward.low();
+        Thread.sleep(300);
+        backward.high();
+        forward.low();
+        Thread.sleep(20);
+        forward.high();
+        right.high();
     }
 
     /**
@@ -47,29 +115,17 @@ public class RoverApplication {
         backward.low();
     }
 
-    @RequestMapping("/")
-    public void all() {
+    private void rightForward() throws InterruptedException {
         initialize();
+        right.low();
+        Thread.sleep(100);
+        forward.low();
+        Thread.sleep(300);
         forward.high();
-        left.high();
+        backward.low();
+        Thread.sleep(50);
+        backward.high();
         right.high();
     }
 
-    @RequestMapping("/green")
-    public void green() {
-        initialize();
-        right.high();
-    }
-
-    @RequestMapping("/yellow")
-    public void yellow() {
-        initialize();
-        left.high();
-    }
-
-    @RequestMapping("/red")
-    public void red() {
-        initialize();
-        forward.high();
-    }
 }
